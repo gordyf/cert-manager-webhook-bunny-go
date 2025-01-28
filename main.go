1a650e36b4b90c24514ad2386f71ae490f1f2e52
Gordy French <gordyf@gordyf.com>
2025-01-27 23:13:46 -0800

    it works

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 
-	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/client-go/rest"
 
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
@@ -13,10 +18,20 @@ import (
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
+var ApiKey = os.Getenv("API_KEY")
+
+const (
+	bunnyAPIBase = "https://api.bunny.net"
+	recordTTL    = 10
+	recordType   = 3
+)
 
 func main() {
 	if GroupName == "" {
 		panic("GROUP_NAME must be specified")
+	}
+	if ApiKey == "" {
+		panic("API_KEY must be specified")
 	}
 
 	// This will register our custom DNS provider with the webhook serving
@@ -25,15 +40,15 @@ func main() {
 	// webhook, where the Name() method will be used to disambiguate between
 	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
-		&customDNSProviderSolver{},
+		&bunnyNetDNSSolver{},
 	)
 }
 
-// customDNSProviderSolver implements the provider-specific logic needed to
+// bunnyNetDNSSolver implements the provider-specific logic needed to
 // 'present' an ACME challenge TXT record for your own DNS provider.
 // To do so, it must implement the `github.com/cert-manager/cert-manager/pkg/acme/webhook.Solver`
 // interface.
-type customDNSProviderSolver struct {
+type bunnyNetDNSSolver struct {
 	// If a Kubernetes 'clientset' is needed, you must:
 	// 1. uncomment the additional `client` field in this structure below
 	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
@@ -43,7 +58,7 @@ type customDNSProviderSolver struct {
 	//client kubernetes.Clientset
 }
 
-// customDNSProviderConfig is a structure that is used to decode into when
+// bunnyNetDNSConfig is a structure that is used to decode into when
 // solving a DNS01 challenge.
 // This information is provided by cert-manager, and may be a reference to
 // additional configuration that's needed to solve the challenge for this
@@ -57,7 +72,7 @@ type customDNSProviderSolver struct {
 // You should not include sensitive information here. If credentials need to
 // be used by your provider here, you should reference a Kubernetes Secret
 // resource and fetch these credentials using a Kubernetes clientset.
-type customDNSProviderConfig struct {
+type bunnyNetDNSConfig struct {
 	// Change the two fields below according to the format of the configuration
 	// to be decoded.
 	// These fields will be set by users in the
@@ -65,6 +80,7 @@ type customDNSProviderConfig struct {
 
 	//Email           string `json:"email"`
 	//APIKeySecretRef v1alpha1.SecretKeySelector `json:"apiKeySecretRef"`
+	APIKey string
 }
 
 // Name is used as the name for this DNS solver when referencing it on the ACME
@@ -73,26 +89,101 @@ type customDNSProviderConfig struct {
 // solvers configured with the same Name() **so long as they do not co-exist
 // within a single webhook deployment**.
 // For example, `cloudflare` may be used as the name of a solver.
-func (c *customDNSProviderSolver) Name() string {
-	return "my-custom-solver"
+func (c *bunnyNetDNSSolver) Name() string {
+	return "bunny-net"
 }
 
-// Present is responsible for actually presenting the DNS record with the
-// DNS provider.
-// This method should tolerate being called multiple times with the same value.
-// cert-manager itself will later perform a self check to ensure that the
-// solver has correctly configured the DNS provider.
-func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
-	cfg, err := loadConfig(ch.Config)
+// Present creates a DNS record to prove domain ownership.
+// It implements the ACME DNS01 challenge verification.
+func (c *bunnyNetDNSSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to load config: %w", err)
 	}
 
-	// TODO: do something more useful with the decoded configuration
-	fmt.Printf("Decoded configuration %v", cfg)
+	zoneID, err := GetZoneID(ch.ResolvedZone, cfg)
+	if err != nil {
+		return fmt.Errorf("failed to get zone ID: %w", err)
+	}
 
-	// TODO: add code that sets a record in the DNS provider's console
+	url := fmt.Sprintf("%s/dnszone/%d/records", bunnyAPIBase, zoneID)
+
+	hostname := strings.TrimSuffix(ch.ResolvedFQDN, ch.ResolvedZone)
+	hostname = strings.TrimSuffix(hostname, ".")
+
+	record := struct {
+		Type     int    `json:"Type"`
+		TTL      int    `json:"Ttl"`
+		Value    string `json:"Value"`
+		Name     string `json:"Name"`
+		Disabled bool   `json:"Disabled"`
+	}{
+		Type:     recordType,
+		TTL:      recordTTL,
+		Value:    ch.Key,
+		Name:     hostname,
+		Disabled: false,
+	}
+
+	payload, err := json.Marshal(record)
+	if err != nil {
+		return fmt.Errorf("failed to marshal record: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(payload))
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("AccessKey", ApiKey)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response: %w", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("API request failed with status %d: %s", resp.StatusCode, string(body))
+	}
+
+	log.Printf("Successfully created DNS record for %s", ch.ResolvedFQDN)
 	return nil
+}
+
+func GetZone(zone string, cfg bunnyNetDNSConfig) (Root, error) {
+	if zone[len(zone)-1] == '.' {
+		zone = zone[:len(zone)-1]
+	}
+	url := fmt.Sprintf(`https://api.bunny.net/dnszone?page=1&perPage=1&search=%s`, zone)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("AccessKey", cfg.APIKey)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var data Root
+	json.Unmarshal([]byte(body), &data)
+	return data, nil
+}
+
+func GetZoneID(zone string, cfg bunnyNetDNSConfig) (int64, error) {
+	data, err := GetZone(zone, cfg)
+	if err != nil {
+		return 0, err
+	}
+	return int64(data.Items[0].ID), nil
 }
 
 // CleanUp should delete the relevant TXT record from the DNS provider console.
@@ -101,9 +192,40 @@ func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 // value provided on the ChallengeRequest should be cleaned up.
 // This is in order to facilitate multiple DNS validations for the same domain
 // concurrently.
-func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
-	// TODO: add code that deletes a record from the DNS provider's console
-	return nil
+func (c *bunnyNetDNSSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig()
+	if err != nil {
+		return err
+	}
+
+	zoneData, err := GetZone(ch.ResolvedZone, cfg)
+	if err != nil {
+		return fmt.Errorf("failed to get zone ID: %w", err)
+	}
+
+	// get matching record id
+	recordID := 0
+	hostname := strings.TrimSuffix(strings.TrimSuffix(ch.ResolvedFQDN, ch.ResolvedZone), ".")
+	for _, record := range zoneData.Items[0].Records {
+		if record.Type == 3 && record.Name == hostname {
+			recordID = record.ID
+			break
+		}
+	}
+	if recordID == 0 {
+		return fmt.Errorf("record not found")
+	}
+
+	url := fmt.Sprintf("%s/dnszone/%d/records/%d", bunnyAPIBase, zoneData.Items[0].ID, recordID)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("AccessKey", cfg.APIKey)
+	_, err = http.DefaultClient.Do(req)
+	return err
 }
 
 // Initialize will be called when the webhook first starts.
@@ -115,7 +237,7 @@ func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
 // provider accounts.
 // The stopCh can be used to handle early termination of the webhook, in cases
 // where a SIGTERM or similar signal is sent to the webhook process.
-func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+func (c *bunnyNetDNSSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
 	///// UNCOMMENT THE BELOW CODE TO MAKE A KUBERNETES CLIENTSET AVAILABLE TO
 	///// YOUR CUSTOM DNS PROVIDER
 
@@ -132,15 +254,75 @@ func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stop
 
 // loadConfig is a small helper function that decodes JSON configuration into
 // the typed config struct.
-func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
-	cfg := customDNSProviderConfig{}
-	// handle the 'base case' where no configuration has been provided
-	if cfgJSON == nil {
-		return cfg, nil
+func loadConfig() (bunnyNetDNSConfig, error) {
+	if ApiKey == "" {
+		panic("API_KEY must be specified")
 	}
-	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
-		return cfg, fmt.Errorf("error decoding solver config: %v", err)
+	cfg := bunnyNetDNSConfig{
+		APIKey: ApiKey,
 	}
 
 	return cfg, nil
+}
+
+type Root struct {
+	Items        []Item `json:"Items"`
+	CurrentPage  int    `json:"CurrentPage"`
+	TotalItems   int    `json:"TotalItems"`
+	HasMoreItems bool   `json:"HasMoreItems"`
+}
+
+// Item represents an item in the Items array
+type Item struct {
+	ID                            int       `json:"Id"`
+	Domain                        string    `json:"Domain"`
+	Records                       []Record  `json:"Records"`
+	DateModified                  time.Time `json:"DateModified"`
+	DateCreated                   time.Time `json:"DateCreated"`
+	NameserversDetected           bool      `json:"NameserversDetected"`
+	CustomNameserversEnabled      bool      `json:"CustomNameserversEnabled"`
+	Nameserver1                   string    `json:"Nameserver1"`
+	Nameserver2                   string    `json:"Nameserver2"`
+	SoaEmail                      string    `json:"SoaEmail"`
+	NameserversNextCheck          time.Time `json:"NameserversNextCheck"`
+	LoggingEnabled                bool      `json:"LoggingEnabled"`
+	LoggingIPAnonymizationEnabled bool      `json:"LoggingIPAnonymizationEnabled"`
+	LogAnonymizationType          int       `json:"LogAnonymizationType"`
+}
+
+// Record represents a DNS record
+type Record struct {
+	ID                    int            `json:"Id"`
+	Type                  int            `json:"Type"`
+	Ttl                   int            `json:"Ttl"`
+	Value                 string         `json:"Value"`
+	Name                  string         `json:"Name"`
+	Weight                int            `json:"Weight"`
+	Priority              int            `json:"Priority"`
+	Port                  int            `json:"Port"`
+	Flags                 int            `json:"Flags"`
+	Tag                   string         `json:"Tag"`
+	Accelerated           bool           `json:"Accelerated"`
+	AcceleratedPullZoneId int            `json:"AcceleratedPullZoneId"`
+	LinkName              string         `json:"LinkName"`
+	IPGeoLocationInfo     *IPGeoLocation `json:"IPGeoLocationInfo"`
+	GeolocationInfo       interface{}    `json:"GeolocationInfo"`
+	MonitorStatus         int            `json:"MonitorStatus"`
+	MonitorType           int            `json:"MonitorType"`
+	GeolocationLatitude   float64        `json:"GeolocationLatitude"`
+	GeolocationLongitude  float64        `json:"GeolocationLongitude"`
+	EnviromentalVariables []interface{}  `json:"EnviromentalVariables"`
+	LatencyZone           *string        `json:"LatencyZone"`
+	SmartRoutingType      int            `json:"SmartRoutingType"`
+	Disabled              bool           `json:"Disabled"`
+	Comment               *string        `json:"Comment"`
+}
+
+// IPGeoLocation represents the IP geolocation information
+type IPGeoLocation struct {
+	CountryCode      string `json:"CountryCode"`
+	Country          string `json:"Country"`
+	ASN              int    `json:"ASN"`
+	OrganizationName string `json:"OrganizationName"`
+	City             string `json:"City"`
 }
